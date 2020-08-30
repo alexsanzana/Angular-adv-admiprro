bb82677438b42432addc6bd0f0275e3eb12260d1
Alex Sanzana <alexsanzana1@gmail.com>
2020-08-30 15:51:12 -0400

    Fin seccion 6

--- src/app/pages/grafica1/grafica1.component.ts
+++ src/app/pages/grafica1/grafica1.component.ts
@@ -1,15 +1,15 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-grafica1',
   templateUrl: './grafica1.component.html',
   styles: []
 })
-export class Grafica1Component implements OnInit {
+export class Grafica1Component {
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
+  public labels1: string[] = ['Pan', ' Refrescos', 'tacos'];
+  public data1 = [
+    [10, 15, 40],
+  ];
 
 }
